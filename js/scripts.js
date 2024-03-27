1b790c86c369148c8393a188d94703db0f934fb1
ART89-prog <dykpgilkom@mail.ru>
2024-03-28 00:14:08 +0300

    58465784784684

--- js/scripts.js
+++ js/scripts.js
@@ -59,8 +59,6 @@ $(() => {
 	// }
 
 
-  $('select').niceSelect();
-
 
   $('input[type=tel]').inputmask('+7 (999) 999-99-99')
 
@@ -88,139 +86,26 @@ $(() => {
 		return false;
 	});
 
-      // Слайдер Услуги
 
-      const serviceSliders = [],
-      service = document.querySelectorAll('.service .swiper')
-  
-      service.forEach(function (el, i) {
-      el.classList.add('service_s' + i)
-  
-      let options = {
-        loop: true,
-        speed: 500,
-        watchSlidesProgress: true,
-        slideActiveClass: 'active',
-        slideVisibleClass: 'visible',
-        pagination: {
-          el: '.swiper-pagination',
-          type: 'bullets',
-          clickable: true,
-        },
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev'
-        },
-        preloadImages: false,
-        lazy: {
-          enabled: true,
-          checkInView: true,
-          loadOnTransitionStart: true,
-          loadPrevNext: true
-        },
-        breakpoints: {
-          0: {
-            spaceBetween: 0,
-            slidesPerView: 1
-          },
-          480: {
-            spaceBetween: 0,
-            slidesPerView: 1
-          },
-          768: {
-            spaceBetween: 20,
-            slidesPerView: 2
-          },
-          1280: {
-            spaceBetween: 30,
-            slidesPerView: 3
-          }
-        },
-        on: {
-          init: swiper => {
-            setTimeout(() => setHeight($(swiper.$el).find('.swiper-slide')))
-          },
-          resize: swiper => {
-            setTimeout(() => {
-              $(swiper.$el).find('.swiper-slide').height('auto')
-              setHeight($(swiper.$el).find('.swiper-slide'))
-            })
-          }
-        }
+
+  const swiper = new Swiper('.work .swiper', {
+    slidesPerView: 2,
+    spaceBetween: 40,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 30
+      },
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 40
       }
-  
-      serviceSliders.push(new Swiper('.service_s' + i, options))
-    })
-
-
-
-     // Слайдер Работы
-
-     const worksSliders = [],
-     works = document.querySelectorAll('.works .swiper')
- 
-     works.forEach(function (el, i) {
-     el.classList.add('works_s' + i)
- 
-     let options = {
-       loop: true,
-       speed: 500,
-       watchSlidesProgress: true,
-       slideActiveClass: 'active',
-       slideVisibleClass: 'visible',
-       pagination: {
-         el: '.swiper-pagination2',
-         type: 'bullets',
-         clickable: true,
-       },
-       navigation: {
-         nextEl: '.swiper-button-next2',
-         prevEl: '.swiper-button-prev2'
-       },
-       preloadImages: false,
-       lazy: {
-         enabled: true,
-         checkInView: true,
-         loadOnTransitionStart: true,
-         loadPrevNext: true
-       },
-       breakpoints: {
-         0: {
-           spaceBetween: 20,
-           slidesPerView: 2
-         },
-         480: {
-           spaceBetween: 20,
-           slidesPerView: 2
-         },
-         768: {
-           spaceBetween: 20,
-           slidesPerView: 3
-         },
-         1023: {
-          spaceBetween: 20,
-          slidesPerView: 3
-        },
-         1280: {
-           spaceBetween: 30,
-           slidesPerView: 4
-         }
-       },
-       on: {
-         init: swiper => {
-           setTimeout(() => setHeight($(swiper.$el).find('works .swiper-slide')))
-         },
-         resize: swiper => {
-           setTimeout(() => {
-             $(swiper.$el).find('.works .swiper-slide').height('auto')
-             setHeight($(swiper.$el).find('.works .swiper-slide'))
-           })
-         }
-       }
-     }
- 
-     worksSliders.push(new Swiper('.works_s' + i, options))
-   })
+    }
+  })
 
 
 
