40264f68e4e29a6227f8118b58993c63ec54a075
ART89-prog <dykpgilkom@mail.ru>
2024-03-29 00:00:08 +0300

    563462567768768

--- js/scripts.js
+++ js/scripts.js
@@ -117,9 +117,13 @@ $(() => {
       },
       480: {
         slidesPerView: 1,
-        spaceBetween: 30
+        spaceBetween: 20
       },
       640: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      1023: {
         slidesPerView: 2,
         spaceBetween: 40
       }
